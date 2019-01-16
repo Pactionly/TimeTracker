e66aa19e5758e75823af655490c7258724925254
Jordan A <jordana@Jordans-MacBook-Pro.local>
2019-01-16 12:08:11 -0800

    Able to sign in with google (not sure of extent of this sign in

--- TimeTracker/TimeTracker/settings.py
+++ TimeTracker/TimeTracker/settings.py
@@ -39,7 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    
+    'allauth.socialaccount.providers.google',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -68,6 +78,14 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 WSGI_APPLICATION = 'TimeTracker.wsgi.application'
 
--- TimeTracker/TimeTracker/urls.py
+++ TimeTracker/TimeTracker/urls.py
@@ -20,5 +20,8 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('myapp.urls')),
     path('admin/', admin.site.urls),
-    path('', include('calendarpage.urls'))
+    path('', include('calendarpage.urls')),
+    path('accounts/', include('allauth.urls')),
+#    url(r'^accounts/', include('allauth.urls')),
+
 ]
