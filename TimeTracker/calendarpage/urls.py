ae065cf8f6d673a6de28b5689776a18ce096ce8f
Jordan A <jordana@Jordans-MacBook-Pro.local>
2019-01-16 15:32:35 -0800

    Login goes to calendar display page

--- TimeTracker/calendarpage/urls.py
+++ TimeTracker/calendarpage/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('calendar/', views.mycalendar, name='mycalendar'),
     path('calendarX/', views.mcx, name ='mcx'),
+    path('accounts/profile/', views.mcx, name ='mcx'),
 ]
          
