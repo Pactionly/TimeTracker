ff4d102b14a5a89ca8d412f3828ce2e87e05f013
gesparza3 <gesparza3@mail.csuchico.edu>
2019-01-17 12:50:06 -0800

    Fixed issues with code for linting

--- TimeTracker/calendarpage/views.py
+++ TimeTracker/calendarpage/views.py
@@ -1,15 +1,19 @@
+"""Render view templates"""
 from django.shortcuts import render
 
 from django.http import HttpResponse
-     
+
 def index(request):
+    """HTTP Response"""
     return HttpResponse("<h1> CALENDAR PAGE")
 
 def mycalendar(request):
+    """Render calendar template"""
     context = {}
     return render(request, 'calendar.html', context)
 
 def mcx(request):
+    """Render mcx template"""
     context = {}
     return render(request, 'mcx.html', context)
 
--- TimeTracker/myapp/forms.py
+++ TimeTracker/myapp/forms.py
@@ -1,21 +1,21 @@
+"""
+Registration form
+"""
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-  email = forms.EmailField(
-    label="Email",
-    required=True
-  )
-  class Meta:
-     model = User
-     fields = ("username", "email", "password1", "password2")
+    """Email label"""
+    email = forms.EmailField(
+        label="Email",
+        required=True
+    )
 
-  def save(self, commit=True):
-    user = super(RegistrationForm, self).save(commit=False)
-    user.email = self.cleaned_data["email"]
-    if commit:
-      user.save()
-    return user
-
+    def save(self, commit=True):
+        """Save metadata"""
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
