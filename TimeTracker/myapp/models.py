de07892ea75156d1bb2d9340835468f1cc6b558c
Jonathan Jones <21381891+Pactionly@users.noreply.github.com>
2019-01-17 13:45:34 -0800

    Minor Linting fixes

--- TimeTracker/myapp/models.py
+++ TimeTracker/myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 class AuthModel(models.Model):
-  id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='AuthModel')
-  refresh_key = models.CharField(max_length=100)
+    """Holds Google Auth Token"""
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='AuthModel')
+    refresh_key = models.CharField(max_length=100)
--- TimeTracker/myapp/views.py
+++ TimeTracker/myapp/views.py
@@ -37,59 +37,55 @@ def logout_view(request):
 
 @login_required
 def sheets_view(request):
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-    '/code/client_secret',
-    scopes=['https://www.googleapis.com/auth/drive']
-  )
-  flow.redirect_uri = 'http://localhost:8000/oauth2callback'
-  authorization_url, state = flow.authorization_url(
-    access_type='offline',
-    include_granted_scopes='true'
-  )
-  return redirect(authorization_url)
+    """Google Authentication"""
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      '/code/client_secret',
+      scopes=['https://www.googleapis.com/auth/drive']
+    )
+    flow.redirect_uri = 'http://localhost:8000/oauth2callback'
+    authorization_url, state = flow.authorization_url(
+      access_type='offline',
+      include_granted_scopes='true'
+    )
+    return redirect(authorization_url)
 
 @login_required
 def sheets_auth(request):
-  state = request.GET.get('state', None)
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-    '/code/client_secret',
-    scopes=['https://www.googleapis.com/auth/drive'],
-    state=state
-  )
-  code = request.GET.get('code', None)
-  flow.redirect_uri = 'http://localhost:8000/oauth2callback'
-  authorization_response = request.build_absolute_uri()        
-  flow.fetch_token(code=code) 
-  credentials = flow.credentials
-
-  if credentials.refresh_token:
-    token = models.AuthModel(
-      id = request.user,
-      refresh_key = credentials.refresh_token
+    """Called After Google Auth"""
+    state = request.GET.get('state', None)
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      '/code/client_secret',
+      scopes=['https://www.googleapis.com/auth/drive'],
+      state=state
     )
-    token.save()
-  credentials = client.OAuth2Credentials(
-    access_token = None,
-    client_id = credentials.client_id,
-    client_secret = credentials.client_secret,
-    refresh_token = request.user.AuthModel.refresh_key,
-    token_expiry = None,
-    token_uri = GOOGLE_TOKEN_URI,
-    user_agent = None,
-    revoke_uri = GOOGLE_REVOKE_URI
-  )
-  print("cred")
-  print(credentials.refresh_token)
-  print(credentials.token_uri)
-  print(credentials.client_id)
-  print(credentials.client_secret)
-  credentials.refresh(httplib2.Http())
-  service = build('sheets', 'v4', credentials=credentials)
-  # Call the Sheets API
-  file_id = '1JuoSIWuGtA5S_KDx4NX7f_eJs_GsdgO5W0rjdzQgNVg'
-  sheet = service.spreadsheets()
-  range = 'Sheet2!B3:E3'
-  body = {'values':[['1/15', 'Testing Output', 'TestHours', 'Debug']] }
-  result = sheet.values().update(spreadsheetId=file_id, valueInputOption='USER_ENTERED', range=range, body=body).execute()
+    code = request.GET.get('code', None)
+    flow.redirect_uri = 'http://localhost:8000/oauth2callback'
+    authorization_response = request.build_absolute_uri()        
+    flow.fetch_token(code=code) 
+    credentials = flow.credentials
 
-  return redirect('/')
+    if credentials.refresh_token:
+        token = models.AuthModel(
+          id = request.user,
+          refresh_key = credentials.refresh_token
+        )
+        token.save()
+    credentials = client.OAuth2Credentials(
+      access_token = None,
+      client_id = credentials.client_id,
+      client_secret = credentials.client_secret,
+      refresh_token = request.user.AuthModel.refresh_key,
+      token_expiry = None,
+      token_uri = GOOGLE_TOKEN_URI,
+      user_agent = None,
+      revoke_uri = GOOGLE_REVOKE_URI
+    )
+    service = build('sheets', 'v4', credentials=credentials)
+    # Call the Sheets API
+    file_id = '1JuoSIWuGtA5S_KDx4NX7f_eJs_GsdgO5W0rjdzQgNVg'
+    sheet = service.spreadsheets()
+    range = 'Sheet2!B3:E3'
+    body = {'values':[['1/15', 'Testing Output', 'TestHours', 'Debug']] }
+    result = sheet.values().update(spreadsheetId=file_id, valueInputOption='USER_ENTERED', range=range, body=body).execute()
+
+    return redirect('/')
