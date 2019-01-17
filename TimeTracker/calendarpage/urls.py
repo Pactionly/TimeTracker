fb7274cf8999afbcd0c9f87d357a18d35258734a
Sean Jordan <seanj@liatrio.com>
2019-01-16 16:18:52 -0800

    fixing base.html calendar link

--- TimeTracker/calendarpage/urls.py
+++ TimeTracker/calendarpage/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('calendarX/', views.mcx, name ='mcx'),
+    path('x/', views.mcx, name ='mcx'),
     path('accounts/profile/', views.mcx, name ='mcx'),
     path('', views.mycalendar, name='mycalendar'),
 ]
