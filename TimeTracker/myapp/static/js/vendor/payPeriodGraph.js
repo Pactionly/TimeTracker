0cbeede1013d376639c8d2ce67292885837a91c6
Sean Jordan <seanj@liatrio.com>
2019-01-25 16:58:17 -0800

    Migrated index.js into payPeriodGraph.js, added logic to graphs to render real data, added the sheetID to display on the profile page

--- TimeTracker/myapp/static/js/vendor/payPeriodGraph.js
+++ TimeTracker/myapp/static/js/vendor/payPeriodGraph.js
@@ -1,26 +1,43 @@
-window.onload = function () {
+var xhttp = new XMLHttpRequest();
 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	title:{
-		text: "Hours worked this pay period"
-	},
-	axisY:{
-		title: "Hours Worked"
-	},
-	data: [{
-		type: "column",
-		name: "Avg. Lifespan",
-		dataPoints: [
-			{ y: 2, label: "1/19/19" },
-			{ y: 4, label: "1/20/19" },
-			{ y: 7, label: "1/21/19" },
-			{ y: 6, label: "1/22/19" },
-			{ y: 4, label: "1/23/19" },
-			{ y: 8, label: "1/24/19" },
-			{ y: 5, label: "1/25/19" }
-		]
-	}]
-});
-chart.render();
+xhttp.onreadystatechange = function()
+{
+  var info = JSON.parse(this.responseText);
+  updatePage(info);
 }
+
+
+function updatePage(info)
+{
+  var totalHours = document.getElementById("totalHours");
+  totalHours.innerText = info.hours;
+
+  var lastFiveDays = info.last_five_days;
+  // Use this variable for calander
+  var d = [];
+  for(var i = 0; i < lastFiveDays.length; i++)
+  {
+    d.push({ y: lastFiveDays[i].hours, label: lastFiveDays[i].date });
+  }
+
+  var chart = new CanvasJS.Chart("chartContainer", {
+  	animationEnabled: true,
+  	title:{
+  		text: "Hours worked this pay period"
+  	},
+  	axisY:{
+  		title: "Hours Worked"
+  	},
+  	data: [{
+  		type: "column",
+  		name: "Hours",
+  		dataPoints: d
+  	}]
+  });
+
+
+  chart.render();
+}
+
+xhttp.open("GET", "/work_stats/", true);
+xhttp.send();
