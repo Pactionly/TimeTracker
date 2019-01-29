3d30f88deaefab11105e78717304e985078a9620
Jonathan Jones <21381891+Pactionly@users.noreply.github.com>
2019-01-29 10:54:02 -0800

    Fixes lint problems

--- TimeTracker/myapp/tests.py
+++ TimeTracker/myapp/tests.py
@@ -1,7 +1,5 @@
 """Defines Tests For myapp"""
 
-import json
-
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
@@ -10,8 +8,7 @@ class IndexViewTests(TestCase):
     """ Tests for index """
     def setUp(self):
         """ Setup run before tests """
-        User = get_user_model()
-        user = User.objects.create_user('temp', 'temp@gmail.com', 'temp')
+        get_user_model().objects.create_user('temp', 'temp@gmail.com', 'temp')
 
     def test_page_redirect_no_login(self):
         """ If not logged in, page should redirect """
@@ -20,7 +17,6 @@ class IndexViewTests(TestCase):
 
     def test_page_load_with_login(self):
         """ If logged in, page should load properly """
-        User = get_user_model()
         self.client.login(username='temp', password='temp')
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -29,8 +25,7 @@ class ProfileViewTests(TestCase):
     """ Tests for /profile/ """
     def setUp(self):
         """ Setup Run Before Tests """
-        User = get_user_model()
-        user = User.objects.create_user('temp', 'temp@gmail.com', 'temp')
+        get_user_model().objects.create_user('temp', 'temp@gmail.com', 'temp')
 
     def test_page_redirect_no_login(self):
         """ If not logged in page should redirect """
@@ -39,7 +34,6 @@ class ProfileViewTests(TestCase):
 
     def test_page_load_with_login(self):
         """ If logged in page should load properly """
-        User = get_user_model()
         self.client.login(username='temp', password='temp')
         response = self.client.get('/profile/')
         self.assertEqual(response.status_code, 200)
@@ -48,8 +42,7 @@ class WorkStatsViewTests(TestCase):
     """ Tests For /work_stats/ """
     def setUp(self):
         """Setup Run Before Tests"""
-        User = get_user_model()
-        user = User.objects.create_user('temp', 'temp@gmail.com', 'temp')
+        get_user_model().objects.create_user('temp', 'temp@gmail.com', 'temp')
 
     def test_page_redirect_no_login(self):
         """ If not logged in page should redirect """
@@ -58,7 +51,6 @@ class WorkStatsViewTests(TestCase):
 
     def test_page_load_with_login(self):
         """ If logged in page should load properly """
-        User = get_user_model()
         self.client.login(username='temp', password='temp')
         response = self.client.get('/work_stats/')
         self.assertEqual(response.status_code, 200)
@@ -68,8 +60,7 @@ class ClockInViewTests(TestCase):
     """ Tests For /clock_in/ """
     def setUp(self):
         """Setup Run Before Tests"""
-        User = get_user_model()
-        user = User.objects.create_user('temp', 'temp@gmail.com', 'temp')
+        get_user_model().objects.create_user('temp', 'temp@gmail.com', 'temp')
 
     def test_page_redirect_no_login(self):
         """ If not logged in page should redirect """
@@ -78,18 +69,14 @@ class ClockInViewTests(TestCase):
 
     def test_page_error_with_get_request(self):
         """ Get Requests should be invalid for this page """
-        User = get_user_model()
         self.client.login(username='temp', password='temp')
         response = self.client.get('/clock_in/')
         self.assertEqual(response.status_code, 400)
 
     def test_page_updates_clock_in_with_post(self):
         """ Page should update database model on post request """
-        User = get_user_model()
         self.client.login(username='temp', password='temp')
         response = self.client.post('/clock_in/', {})
         clock_in_time = User.objects.get(username='temp').profile.clock_in_time
         self.assertEqual(response.status_code, 302)
         self.assertTrue(clock_in_time is not None)
-
-
